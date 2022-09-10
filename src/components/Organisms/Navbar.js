96b5d3962b41aa2862171d5376d71e6b7b8d924d
Hernando17 <ginkotsu17@gmail.com>
2022-09-10 23:24:37 +0700

    Navbar Update

--- src/components/Organisms/Navbar.js
+++ src/components/Organisms/Navbar.js
@@ -2,14 +2,34 @@ import React from "react";
 import "../../assets/css/style.css";
 
 export default function Navbar() {
+  const [navUl, setNavUl] = React.useState(false);
+
   return (
     <nav className="navbar">
-      <div className="navbar-item">
-        <a href="#about">About</a>
-        <a>Contact</a>
-        <a>Education</a>
-        <a>Career</a>
-        <a>Project</a>
+      <ul className={navUl ? "active" : ""}>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+        <li>
+          <a>Education</a>
+        </li>
+        <li>
+          <a>Career</a>
+        </li>
+        <li>
+          <a>Project</a>
+        </li>
+      </ul>
+      <div
+        className="hamburger"
+        onClick={() => (navUl ? setNavUl(false) : setNavUl(true))}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </nav>
   );
