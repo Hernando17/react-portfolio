91381d1d774bec750c8f233d941b51190e52b8d4
Hernando17 <ginkotsu17@gmail.com>
2022-09-11 15:52:20 +0700

    Navbar Responsive Icon

--- src/components/Organisms/Navbar.js
+++ src/components/Organisms/Navbar.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "../../assets/css/style.css";
 import { useNavigate } from "react-router-dom";
+import {
+  FaHome,
+  FaUserAlt,
+  FaPhoneAlt,
+  FaUserTie,
+  FaRegFileCode,
+} from "react-icons/fa";
 
 export default function Navbar() {
-  const [navUl, setNavUl] = React.useState(false);
   let navigate = useNavigate();
 
   return (
     <div className="center">
       <nav className="navbar">
-        <ul className={navUl ? "active" : ""}>
+        <ul className="desktop">
           <li>
             <a onClick={() => navigate(`/`)}>Main</a>
           </li>
@@ -24,6 +30,33 @@ export default function Navbar() {
           </li>
           <li>
             <a>Project</a>
+          </li>
+        </ul>
+        <ul className="mobile">
+          <li>
+            <a onClick={() => navigate(`/`)}>
+              <FaHome />
+            </a>
+          </li>
+          <li>
+            <a onClick={() => navigate(`/about`)}>
+              <FaUserAlt />
+            </a>
+          </li>
+          <li>
+            <a onClick={() => navigate(`/contact`)}>
+              <FaPhoneAlt />
+            </a>
+          </li>
+          <li>
+            <a>
+              <FaUserTie />
+            </a>
+          </li>
+          <li>
+            <a>
+              <FaRegFileCode />
+            </a>
           </li>
         </ul>
       </nav>
