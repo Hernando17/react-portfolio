24842ede3df5b7cd01bdf505cf59b0baf06204e2
Hernando17 <ginkotsu17@gmail.com>
2022-12-08 12:07:34 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -4,26 +4,27 @@ import "./assets/css/style.scss";
 import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
-  const [loading, setLoading] = React.useState(true);
+    const [loading, setLoading] = React.useState(true);
 
-  React.useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  });
+    React.useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    });
 
-  return (
-    <AnimatePresence exitBeforeEnter>
-      {loading ? (
-        <div className="welcome">
-          <h1 style={{ color: "white" }}>WELCOME</h1>
-        </div>
-      ) : (
-        ""
-      )}
-      <Router />
-    </AnimatePresence>
-  );
+    if (loading) {
+        return (
+            <div className="welcome">
+                <h1 style={{ color: "white" }}>WELCOME</h1>
+            </div>
+        );
+    } else {
+        return (
+            <AnimatePresence exitBeforeEnter>
+                <Router />
+            </AnimatePresence>
+        );
+    }
 }
 
 export default App;
