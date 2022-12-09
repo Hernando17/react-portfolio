fa8453e0a5513965a2430d8a7ded7144fef88dfd
Hernando17 <ginkotsu17@gmail.com>
2022-12-09 10:23:16 +0700

    Project Update

--- src/pages/Project/index.js
+++ src/pages/Project/index.js
@@ -1,49 +1,49 @@
 import React from "react";
 import { Navbar } from "../../components";
 import "../../assets/css/style.scss";
-import Telefon from "../../assets/img/project/Telefon.png";
+
 import { project } from "../../resources/project";
 import AOS from "aos";
 
 export default function Project() {
-  React.useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
-  }, []);
+    React.useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            once: true,
+        });
+    }, []);
 
-  return (
-    <div>
-      <Navbar />
-      <div className="project-container">
-        <h1 className="title" data-aos="fade-down">
-          Project
-        </h1>
-        <div className="project-content" data-aos="fade-up">
-          {project.map((data, index) => (
-            <div className="project-card" key={index}>
-              <img src={Telefon} className="project-image" />
-              <div className="card-text">
-                <p>{data.name}</p>
-                <div
-                  className="project-category"
-                  style={{
-                    backgroundColor:
-                      data.category == "Website"
-                        ? "rgb(236, 61, 61)"
-                        : data.category == "Mobile App"
-                        ? "blue"
-                        : "orange",
-                  }}
-                >
-                  <p>{data.category}</p>
+    return (
+        <div>
+            <Navbar />
+            <div className="project-container">
+                <h1 className="title" data-aos="fade-down">
+                    Project
+                </h1>
+                <div className="project-content" data-aos="fade-up">
+                    {project.map((data, index) => (
+                        <div className="project-card" key={index}>
+                            <img src={data.img} className="project-image" />
+                            <div className="card-text">
+                                <p>{data.name}</p>
+                                <div
+                                    className="project-category"
+                                    style={{
+                                        backgroundColor:
+                                            data.category == "Website"
+                                                ? "rgb(236, 61, 61)"
+                                                : data.category == "Mobile App"
+                                                ? "blue"
+                                                : "orange",
+                                    }}
+                                >
+                                    <p>{data.category}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-              </div>
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/resources/project.js
+++ src/resources/project.js
@@ -1,197 +1,219 @@
+import PustakaBangsa from "../assets/img/project/pustakabangsa.png";
+import Telefon from "../assets/img/project/Telefon.png";
+import Walle from "../assets/img/project/wall-e.png";
+import SteamGS from "../assets/img/project/SteamGS.png";
+import EstheticHotel from "../assets/img/project/esthetichotel.png";
+import KrmMotors from "../assets/img/project/krm-motors.png";
+import IlogWeb from "../assets/img/project/ilogweb.png";
+import IlogMobile from "../assets/img/project/ilogmobile.png";
+import ASPApi from "../assets/img/project/aspapi.png";
+import SilajuMobile from "../assets/img/project/silajumobile.png";
+
 export const project = [
-  {
-    name: "Pustaka Bangsa",
-    category: "Website",
-    language: [
-      {
-        name: "Code Igniter",
-      },
-      {
-        name: "PHP",
-      },
-      {
-        name: "Bootstrap",
-      },
-      {
-        name: "MySQL",
-      },
-    ],
-  },
-  {
-    name: "Telefon",
-    category: "Website",
-    language: [
-      {
-        name: "Laravel",
-      },
-      {
-        name: "PHP",
-      },
-      {
-        name: "Bootstrap",
-      },
-      {
-        name: "MySQL",
-      },
-    ],
-  },
-  {
-    name: "Wall-e",
-    category: "Website",
-    language: [
-      {
-        name: "Laravel",
-      },
-      {
-        name: "PHP",
-      },
-      {
-        name: "Bootstrap",
-      },
-      {
-        name: "MySQL",
-      },
-    ],
-  },
-  {
-    name: "Esthetic Hotel",
-    category: "Website",
-    language: [
-      {
-        name: "Code Igniter",
-      },
-      {
-        name: "PHP",
-      },
-      {
-        name: "Bootstrap",
-      },
-      {
-        name: "MySQL",
-      },
-    ],
-  },
-  {
-    name: "KRM Motors",
-    category: "Website",
-    language: [
-      {
-        name: "Laravel",
-      },
-      {
-        name: "PHP",
-      },
-      {
-        name: "Bootstrap",
-      },
-      {
-        name: "MySQL",
-      },
-    ],
-  },
-  {
-    name: "SteamGS",
-    category: "Mobile App",
-    language: [
-      {
-        name: "React Native",
-      },
-      {
-        name: "Javascript",
-      },
-      {
-        name: "Firebase",
-      },
-    ],
-  },
-  {
-    name: "CovID",
-    category: "Mobile App",
-    language: [
-      {
-        name: "React Native",
-      },
-      {
-        name: "Javascript",
-      },
-    ],
-  },
-  {
-    name: "Ilog Mobile",
-    category: "Mobile App",
-    language: [
-      {
-        name: "React Native",
-      },
-      {
-        name: "Javascript",
-      },
-    ],
-  },
-  {
-    name: "Ilog Web",
-    category: "Maintenance",
-    language: [
-      {
-        name: "ASP.NET Core",
-      },
-      {
-        name: "C#",
-      },
-    ],
-  },
-  {
-    name: "Ilog Backend",
-    category: "Maintenance",
-    language: [
-      {
-        name: "ASP.NET Core",
-      },
-      {
-        name: "C#",
-      },
-    ],
-  },
-  {
-    name: "Silaju Mobile",
-    category: "Maintenance",
-    language: [
-      {
-        name: "React Native",
-      },
-      {
-        name: "Javascript",
-      },
-    ],
-  },
-  {
-    name: "Silaju Backend",
-    category: "Maintenance",
-    language: [
-      {
-        name: "ASP.NET Core",
-      },
-      {
-        name: "C#",
-      },
-    ],
-  },
-  {
-    name: "tu-du",
-    category: "Website",
-    language: [
-      {
-        name: "Laravel",
-      },
-      {
-        name: "PHP",
-      },
-      {
-        name: "ReactJS",
-      },
-      {
-        name: "Javascript",
-      },
-    ],
-  },
+    {
+        name: "Pustaka Bangsa",
+        img: PustakaBangsa,
+        category: "Website",
+        language: [
+            {
+                name: "Code Igniter",
+            },
+            {
+                name: "PHP",
+            },
+            {
+                name: "Bootstrap",
+            },
+            {
+                name: "MySQL",
+            },
+        ],
+    },
+    {
+        name: "Telefon",
+        img: Telefon,
+        category: "Website",
+        language: [
+            {
+                name: "Laravel",
+            },
+            {
+                name: "PHP",
+            },
+            {
+                name: "Bootstrap",
+            },
+            {
+                name: "MySQL",
+            },
+        ],
+    },
+    {
+        name: "Wall-e",
+        img: Walle,
+        category: "Website",
+        language: [
+            {
+                name: "Laravel",
+            },
+            {
+                name: "PHP",
+            },
+            {
+                name: "Bootstrap",
+            },
+            {
+                name: "MySQL",
+            },
+        ],
+    },
+    {
+        name: "Esthetic Hotel",
+        img: EstheticHotel,
+        category: "Website",
+        language: [
+            {
+                name: "Code Igniter",
+            },
+            {
+                name: "PHP",
+            },
+            {
+                name: "Bootstrap",
+            },
+            {
+                name: "MySQL",
+            },
+        ],
+    },
+    {
+        name: "KRM Motors",
+        img: KrmMotors,
+        category: "Website",
+        language: [
+            {
+                name: "Laravel",
+            },
+            {
+                name: "PHP",
+            },
+            {
+                name: "Bootstrap",
+            },
+            {
+                name: "MySQL",
+            },
+        ],
+    },
+    {
+        name: "SteamGS",
+        img: SteamGS,
+        category: "Mobile App",
+        language: [
+            {
+                name: "React Native",
+            },
+            {
+                name: "Javascript",
+            },
+            {
+                name: "Firebase",
+            },
+        ],
+    },
+    {
+        name: "CovID",
+        category: "Mobile App",
+        language: [
+            {
+                name: "React Native",
+            },
+            {
+                name: "Javascript",
+            },
+        ],
+    },
+    {
+        name: "Ilog Mobile",
+        img: IlogMobile,
+        category: "Mobile App",
+        language: [
+            {
+                name: "React Native",
+            },
+            {
+                name: "Javascript",
+            },
+        ],
+    },
+    {
+        name: "Ilog Web",
+        img: IlogWeb,
+        category: "Maintenance",
+        language: [
+            {
+                name: "ASP.NET Core",
+            },
+            {
+                name: "C#",
+            },
+        ],
+    },
+    {
+        name: "Ilog Backend",
+        img: ASPApi,
+        category: "Maintenance",
+        language: [
+            {
+                name: "ASP.NET Core",
+            },
+            {
+                name: "C#",
+            },
+        ],
+    },
+    {
+        name: "Silaju Mobile",
+        img: SilajuMobile,
+        category: "Maintenance",
+        language: [
+            {
+                name: "React Native",
+            },
+            {
+                name: "Javascript",
+            },
+        ],
+    },
+    {
+        name: "Silaju Backend",
+        img: ASPApi,
+        category: "Maintenance",
+        language: [
+            {
+                name: "ASP.NET Core",
+            },
+            {
+                name: "C#",
+            },
+        ],
+    },
+    {
+        name: "tu-du",
+        category: "Website",
+        language: [
+            {
+                name: "Laravel",
+            },
+            {
+                name: "PHP",
+            },
+            {
+                name: "ReactJS",
+            },
+            {
+                name: "Javascript",
+            },
+        ],
+    },
 ];
