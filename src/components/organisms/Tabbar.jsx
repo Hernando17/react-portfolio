20f119348df573e67a8657bc73ba5d36763f994e
Hernando <ginkotsu17@gmail.com>
2023-04-12 10:35:07 +0700

    Tabbar update

--- src/components/organisms/Tabbar.jsx
+++ src/components/organisms/Tabbar.jsx
@@ -1,8 +1,40 @@
+import {
+    FaHome,
+    FaUserAlt,
+    FaPhoneAlt,
+    FaUserTie,
+    FaRegFileCode,
+} from "react-icons/fa";
+import { Link, useLocation } from "react-router-dom";
+
+
 export default function Tabbar() {
+    const location = useLocation();
+
     return (
         <nav className="tabbar">
             <div className="container">
-                <h1>tabbar</h1>
+                <ul className="tabbar-content">
+                    <li className={location.pathname === "/" ? "active" : ""} >
+                        <Link to="/" className={location.pathname === "/" ? "active" : ""} >
+                            <FaHome size="18" color={location.pathname === "/" ? "#1e1e1e" : "#fff"} />
+                        </Link>
+                    </li>
+                    <li className={location.pathname === "/about" ? "active" : ""} >
+                        <Link to="/about" className={location.pathname === "/about" ? "active" : ""} >
+                            <FaUserAlt color={location.pathname === "/about" ? "#1e1e1e" : "#fff"} />
+                        </Link>
+                    </li>
+                    <li>
+                        <FaPhoneAlt />
+                    </li>
+                    <li>
+                        <FaUserTie />
+                    </li>
+                    <li>
+                        <FaRegFileCode />
+                    </li>
+                </ul>
             </div>
         </nav>
     )
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import { Landing } from '../pages';
+import { Landing, About } from '../pages';
 
 export default function Routers() {
     return (
         <Routes>
             <Route path="/" element={<Landing />} />
+            <Route path="/about" element={<About />} />
         </Routes>
     )
 }
